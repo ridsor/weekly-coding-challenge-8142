1e0bb40c633777b6a0377f238db18ca28b0de9d8
ridsor <ridsorgamerz@gmail.com>
2023-10-03 20:18:02 +0700

    memperbaiki pesan consolo.log

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let input_number = 6;
 
 for (let i = 1; i <= input_number; i++) {
-  console.log(`Current Number is ${i} and the cube is ${i * i * i}\n`);
+  console.log(`Current Number is : ${i} and the cube is ${i * i * i}\n`);
 }
