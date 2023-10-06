ee695afb66df23c5ca95c431f6cd787e64bf02e3
ridsor <ridsorgamerz@gmail.com>
2023-10-06 16:30:34 +0700

    membuat fitur input dari user

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-let input_number = 6;
+const readline = require("readline");
 
-for (let i = 1; i <= input_number; i++) {
-  console.log(`Current Number is : ${i} and the cube is ${i * i * i}\n`);
-}
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("Masukan angka : ", (input_number) => {
+  for (let i = 1; i <= input_number; i++) {
+    console.log(`Current Number is : ${i} and the cube is ${i ** 3}\n`);
+  }
+  rl.close();
+});
